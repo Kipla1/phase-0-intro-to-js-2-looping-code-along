7090a40917bcd95148395b3b54aca6684daff280
Oscar <oscartrent6@gmail.com>
2025-03-11 11:16:24 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solutions in this file
+// const names = ["Guadalupe", "Ollie", "Aki"]
+// const event = `surprise`
+// function writeCards(names, event){
+//     for (let i = 0; i < names.length ; i++){
+//         console.log(`Thank you ${names[i]} for the wonderful ${event} gift!`)
+//     }
+//     return names;
+// }
+
+
+const names = ["Guadalupe", "Ollie", "Aki"]
+const event = `surprise`
+function writeCards(names, event){
+    let thankYouCards = []
+    for (let i = 0; i < names.length ; i++){
+        thankYouCards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return thankYouCards
+}
+writeCards(["Guadalupe", "Ollie", "Aki"], 'surprise')
+
+
+function countDown(number){
+    while (number >= 0){
+        console.log(number)
+        number--
+    }
+}   
